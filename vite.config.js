c2740a1aac48bc4c01f11abcd2a34d50192a546c
harking6 <1642363459@qq.com>
2024-12-17 17:01:03 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 // vite.config.js
+
+import { fileURLToPath, URL } from 'node:url'
+ 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+ 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  base: './',
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    port: 3000, // 可根据需要更改端口
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
